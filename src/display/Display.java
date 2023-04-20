e7b8548bba175b9e39c4e86e1b4507e636a3d0f7
Олег  Ситкин <oleg@mbp-oleg.dlink>
2023-04-20 21:27:07 +0300

    render TEST

--- src/display/Display.java
+++ src/display/Display.java
@@ -13,13 +13,28 @@ public abstract class Display {
             return;
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas();
+        content = new Canvas(){
+            public void paint(Graphics g){
+                super.paint(g);
+                render(g);
+            }
+        };
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
+        content.setBackground(Color.BLACK);
         window.setResizable(false);
         window.getContentPane().add(content);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
+    public static void render(){
+        content.repaint();
+    }
+
+    private static void render(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillOval(400-50, 300-50, 100, 100);
+    }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,21 @@ package main;
 
 import display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+
 public class Main {
     public static void main(String[] args) {
+
         Display.create(800, 600, "Tanks");
+        Timer t = new Timer(1000/60, new AbstractAction(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Display.render();
+            }
+        });
+        t.setRepeats(true);
+        t.start();
     }
 }
